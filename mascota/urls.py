20677b4e85bee0c610fe3e55ba083514e1d3a349
oozcode <antonio.lankin@gmail.com>
2025-06-12 01:20:33 -0400

    front casi terminado

--- mascota/urls.py
+++ mascota/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path,include
 from .import views
 
-app_name='mascotaf'
+
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('footer/', views.footer, name='footer'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('veterinaria/', views.veterinaria, name='veterinaria'),
+    path('estetica/',views.estetica, name='estetica'),
+    path('movil/', views.movil, name='movil'),
 ]
--- mascota/views.py
+++ mascota/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,'index.html')
+
+def footer(request):
+    return render(request,'footer.html')
+
+def nosotros(request):
+    return render(request,'nosotros.html')
+
+def contacto(request):
+    return render(request,'contacto.html')
+
+def veterinaria(request):
+    return render(request,'veterinaria.html')
+
+def estetica(request):
+    return render(request,'estetica.html')
+
+def movil(request):
+    return render(request,'movil.html')
